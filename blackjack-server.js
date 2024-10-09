21d9bb748410559a91fa672d1e53148039d5d615
conner12345678 <cehly065@west-mec.org>
2024-10-09 13:59:00 -0700

    c-edit-2.5
    forced by diego

--- blackjack-server.js
+++ blackjack-server.js
@@ -6,11 +6,17 @@ const fs = require('fs')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended:true }))
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 const getCards = () => {
     const data = fs.readFileSync(path.join(__dirname, '/data/blackjack-data.json'), 'utf8')
-    return JSON.parse(data)
+    // console.log(data)
+    if(data.length < 1){
+        return []
+    }else{
+        return JSON.parse(data)
+    }
 }
 const saveCards = (data) => {
     fs.writeFileSync('./data/blackjack-data.json', JSON.stringify(data, null, 2), 'utf8')
@@ -19,13 +25,19 @@ const saveCards = (data) => {
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
-app.get('/api/randomhand', async (req,res)=>{
+
+app.get('/game/blackjack', (req,res) => {
+    const cards = getCards()
+    res.render('blackjack', { cards })
+})
+
+app.get('/randomhand', async (req,res)=>{
     const cards = getCards()
     try{
-        const response = await axios.get('https://www.deckofcardsapi.com/api/deck/7mxggn611j3x/draw/?count=2')
-        res.json({
-            data:response.data
-        });
+        const response = await axios.get('https://www.deckofcardsapi.com/api/deck/7mxggn611j3x/draw/?count=1')
+        cards.push(response.data.cards)
+        saveCards(cards)
+        res.redirect('/game/blackjack')
     }
     catch(error){
         res.status(404).send('test failure')
